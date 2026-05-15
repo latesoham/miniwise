9388fa0cb115a20dd6687b51f657b219a0fb80ed
Soham <sohamlate83@gmail.com>
2026-05-15 13:15:33 +0530

    Backend is IN!!

--- src/components/Column.js
+++ src/components/Column.js
@@ -2,40 +2,44 @@ import { useState } from "react";
 import TicketCard from "./TicketCard";
 
 function Column({ title, tickets, moveTicket, deleteTicket, addTicket }) {
-  // 🔹 State
+
   const [input, setInput] = useState("");
   const [priority, setPriority] = useState("Medium");
   const [tag, setTag] = useState("React");
-  const [deadline, setDeadline] = useState("");
 
-  // 🔹 Function
   const handleAdd = () => {
     if (input.trim() === "") return;
-    addTicket(input, priority, tag, deadline);
+
+    addTicket(input, priority, tag);
+
     setInput("");
   };
 
-
-
   return (
-    <div className="bg-white p-4 rounded-2xl shadow-md hover:shadow-lg transition duration-200 w-full max-w-sm min-h-[420px] space-y-3">
-      <h2 className="font-semibold text-lg mb-4 text-gray-700">{title}</h2>
+    <div className="bg-white p-5 rounded-2xl shadow-md hover:shadow-lg transition-all duration-200 w-full max-w-sm min-h-[450px]">
 
-      {/* 🔹 Input Section */}
-      <div className="bg-gray-50 p-3 rounded-xl mb-4 shadow-sm flex flex-col gap-1.5">
+      {/* Header */}
+      <h2 className="text-xl font-semibold text-gray-700 mb-4">
+        {title}
+      </h2>
+
+      {/* Input Section */}
+      <div className="bg-gray-50 p-4 rounded-xl shadow-sm flex flex-col gap-3 mb-5">
+
         <input
-          className="border p-1.5 rounded-lg text-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+          type="text"
+          placeholder="Add task"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          placeholder="Add task"
+          className="border p-2 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
         />
 
-        {/* Row 2 */}
-        <div className="bg-gray-50 p-3 rounded-xl mb-4 shadow-sm flex flex-col gap-1.5">
+        <div className="flex gap-2">
+
           <select
             value={priority}
             onChange={(e) => setPriority(e.target.value)}
-            className="border p-1.5 rounded-lg text-sm bg-white hover:border-blue-400 transition"
+            className="border p-2 rounded-lg text-sm w-1/2 bg-white"
           >
             <option>High</option>
             <option>Medium</option>
@@ -45,57 +49,160 @@ function Column({ title, tickets, moveTicket, deleteTicket, addTicket }) {
           <select
             value={tag}
             onChange={(e) => setTag(e.target.value)}
-            className="border p-1.5 rounded-lg text-sm bg-white hover:border-blue-400 transition"
+            className="border p-2 rounded-lg text-sm w-1/2 bg-white"
           >
             <option>React</option>
-            <option>DSA</option>
             <option>Backend</option>
+            <option>DSA</option>
             <option>UI</option>
           </select>
+
         </div>
 
-        {/* Row 3 */}
-        <div className="bg-gray-50 p-3 rounded-xl mb-4 shadow-sm flex flex-col gap-1.5">
-          <input
-            type="date"
-            value={deadline}
-            onChange={(e) => setDeadline(e.target.value)}
-            className="border p-1 rounded-lg text-sm bg-white"
-          />
+        <button
+          onClick={handleAdd}
+          className="bg-blue-500 hover:bg-blue-600 active:scale-95 transition-all text-white py-2 rounded-lg font-medium"
+        >
+          + Add Task
+        </button>
 
-          <button
-            onClick={handleAdd}
-            className="bg-blue-500 hover:bg-blue-600 active:scale-95 transition transform text-white w-full py-1.5 rounded-lg"
-          >
-            +
-          </button>
-        </div>
       </div>
 
-      {/* 🔹 Tickets Section */}
-      {tickets.map((ticket, index) => (
-        <div key={index}>
-          <TicketCard
-            title={ticket.text}
-            priority={ticket.priority || "Low"}
-            tag={ticket.tag}
-            deadline={ticket.deadline}
-            isDone={title === "Done"}
-            onDelete={() => deleteTicket(index)}
-          />
+      {/* Ticket List */}
+      <div className="space-y-3">
 
-          {moveTicket && (
-            <button
-              onClick={() => moveTicket(index)}
-              className="text-xs text-blue-500 mb-2"
-            >
-              Move →
-            </button>
-          )}
-        </div>
-      ))}
+        {tickets.map((ticket, index) => (
+          <div key={ticket.id || index}>
+
+            <TicketCard
+              title={ticket.title}
+              priority={ticket.priority}
+              tag={ticket.tag}
+              onDelete={() => deleteTicket(ticket.id)}
+            />
+
+            {moveTicket && (
+              <button
+                onClick={() => moveTicket(index)}
+                className="text-xs text-blue-500 hover:text-blue-700 transition"
+              >
+                Move →
+              </button>
+            )}
+
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 }
 
 export default Column;
+
+
+
+
+
+
+// import { useState } from "react";
+// import TicketCard from "./TicketCard";
+
+// function Column({ title, tickets, moveTicket, deleteTicket, addTicket }) {
+//   // 🔹 State
+//   const [input, setInput] = useState("");
+//   const [priority, setPriority] = useState("Medium");
+//   const [tag, setTag] = useState("React");
+//   const [deadline, setDeadline] = useState("");
+
+//   // 🔹 Function
+//   const handleAdd = () => {
+//     if (input.trim() === "") return;
+//     addTicket(input, priority, tag, deadline);
+//     setInput("");
+//   };
+
+
+
+//   return (
+//     <div className="bg-white p-4 rounded-2xl shadow-md hover:shadow-lg transition duration-200 w-full max-w-sm min-h-[420px] space-y-3">
+//       <h2 className="font-semibold text-lg mb-4 text-gray-700">{title}</h2>
+
+//       {/* 🔹 Input Section */}
+//       <div className="bg-gray-50 p-3 rounded-xl mb-4 shadow-sm flex flex-col gap-1.5">
+//         <input
+//           className="border p-1.5 rounded-lg text-sm w-full focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+//           value={input}
+//           onChange={(e) => setInput(e.target.value)}
+//           placeholder="Add task"
+//         />
+
+//         {/* Row 2 */}
+//         <div className="bg-gray-50 p-3 rounded-xl mb-4 shadow-sm flex flex-col gap-1.5">
+//           <select
+//             value={priority}
+//             onChange={(e) => setPriority(e.target.value)}
+//             className="border p-1.5 rounded-lg text-sm bg-white hover:border-blue-400 transition"
+//           >
+//             <option>High</option>
+//             <option>Medium</option>
+//             <option>Low</option>
+//           </select>
+
+//           <select
+//             value={tag}
+//             onChange={(e) => setTag(e.target.value)}
+//             className="border p-1.5 rounded-lg text-sm bg-white hover:border-blue-400 transition"
+//           >
+//             <option>React</option>
+//             <option>DSA</option>
+//             <option>Backend</option>
+//             <option>UI</option>
+//           </select>
+//         </div>
+
+//         {/* Row 3 */}
+//         <div className="bg-gray-50 p-3 rounded-xl mb-4 shadow-sm flex flex-col gap-1.5">
+//           <input
+//             type="date"
+//             value={deadline}
+//             onChange={(e) => setDeadline(e.target.value)}
+//             className="border p-1 rounded-lg text-sm bg-white"
+//           />
+
+//           <button
+//             onClick={handleAdd}
+//             className="bg-blue-500 hover:bg-blue-600 active:scale-95 transition transform text-white w-full py-1.5 rounded-lg"
+//           >
+//             +
+//           </button>
+//         </div>
+//       </div>
+
+//       {/* 🔹 Tickets Section */}
+//       {tickets.map((ticket, index) => (
+//         <div key={index}>
+//           <TicketCard
+//             title={ticket.title}
+//             priority={ticket.priority || "Low"}
+//             tag={ticket.tag}
+//             deadline={ticket.deadline}
+//             isDone={title === "Done"}
+//             onDelete={() => deleteTicket(index)}
+//           />
+
+//           {moveTicket && (
+//             <button
+//               onClick={() => moveTicket(index)}
+//               className="text-xs text-blue-500 mb-2"
+//             >
+//               Move →
+//             </button>
+//           )}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Column;
--- src/components/KanbanBoard.js
+++ src/components/KanbanBoard.js
@@ -1,49 +1,47 @@
-import { tab } from "@testing-library/user-event/dist/tab";
 import Column from "./Column";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function KanbanBoard() {
-  const [tickets, setTickets] = useState(() => {
-    const saved = localStorage.getItem("miniwise-tickets");
-    return saved
-      ? JSON.parse(saved)
-      : {
-          todo: [
-            {
-              text: "Learn React",
-              priority: "High",
-              tag: "Frontend",
-              deadline: "2026-08-10",
-            },
-          ],
-          inProgress: [
-            {
-              text: "Build UI",
-              priority: "Medium",
-              tag: "Forntend",
-              deadline: "2026-07-10",
-            },
-          ],
-          done: [
-            {
-              text: "Project Idea",
-              priority: "Low",
-              tag: "Backend",
-              deadline: "2026-06-10",
-            },
-          ],
-        };
-  });
-  useEffect(() => {
-    localStorage.setItem("miniwise-tickets", JSON.stringify(tickets));
-  }, [tickets]);
 
-  // Move ticket
+  const [tickets, setTickets] = useState({
+    todo: [],
+    inProgress: [],
+    done: [],
+  });
+
+  // Fetch tickets from backend
+  useEffect(() => {
+
+    fetch("http://localhost:8080/tickets")
+      .then((res) => res.json())
+      .then((data) => {
+
+        const organizedTickets = {
+          todo: data.filter((t) => t.status === "Todo"),
+          inProgress: data.filter((t) => t.status === "In Progress"),
+          done: data.filter((t) => t.status === "Done"),
+        };
+
+        setTickets(organizedTickets);
+      })
+      .catch((err) => console.error(err));
+
+  }, []);
+
+  // Move ticket locally
   const moveTicket = (from, to, index) => {
+
     const item = tickets[from][index];
 
+    item.status =
+      to === "todo"
+        ? "Todo"
+        : to === "inProgress"
+        ? "In Progress"
+        : "Done";
+
     const updatedFrom = tickets[from].filter((_, i) => i !== index);
+
     const updatedTo = [...tickets[to], item];
 
     setTickets({
@@ -53,62 +51,111 @@ function KanbanBoard() {
     });
   };
 
-  // Add ticket
-  const addTicketToColumn = (column, text, priority, tag, deadline) => {
-    const newTicket = {
-      text,
-      tag,
-      deadline,
-      priority: priority || "Low",
-    };
+  // Add ticket in sql
+const addTicketToColumn = async (column, title, priority, tag) => {
+
+  const status =
+    column === "todo"
+      ? "Todo"
+      : column === "inProgress"
+      ? "In Progress"
+      : "Done";
+
+  const newTicket = {
+    title,
+    priority,
+    tag,
+    status,
+  };
+
+  try {
+
+    const response = await fetch("http://localhost:8080/tickets", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTicket),
+    });
+
+    const data = await response.json();
 
     setTickets({
       ...tickets,
-      [column]: [...tickets[column], newTicket],
+      [column]: [...tickets[column], data.ticket],
     });
-  };
 
-  // Delete ticket
-  const deleteTicket = (column, index) => {
-    const updated = tickets[column].filter((_, i) => i !== index);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+  // Delete ticket from sql
+const deleteTicket = async (column, ticketId) => {
+
+  try {
+
+    await fetch(`http://localhost:8080/tickets/${ticketId}`, {
+      method: "DELETE",
+    });
+
+    const updated = tickets[column].filter(
+      (ticket) => ticket.id !== ticketId
+    );
 
     setTickets({
       ...tickets,
       [column]: updated,
     });
-  };
 
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+  // Progress calculation
   const totalTasks =
-    tickets.todo.length + tickets.inProgress.length + tickets.done.length;
+    tickets.todo.length +
+    tickets.inProgress.length +
+    tickets.done.length;
 
   const completedTasks = tickets.done.length;
 
   const progress =
-    totalTasks === 0 ? 0 : Math.round((completedTasks / totalTasks) * 100);
+    totalTasks === 0
+      ? 0
+      : Math.round((completedTasks / totalTasks) * 100);
 
   return (
-    <div className="p-6 bg-gray-100 min-h-scren">
+    <div className="p-6 bg-gray-100 min-h-screen">
+
       {/* Progress */}
       <div className="mb-6 max-w-4xl mx-auto">
-        <h2 className="text-lg font-semibold mb-2">Progress: {progress}%</h2>
+
+        <h2 className="text-lg font-semibold mb-2">
+          Progress: {progress}%
+        </h2>
 
         <div className="w-full bg-gray-300 rounded-full h-4">
+
           <div
-            className="h-3 rounded-full transition-all duration-300 bg-gradient-to-r from-green-400 to-green-600"
+            className="h-4 rounded-full transition-all duration-500 bg-gradient-to-r from-green-400 to-green-600"
             style={{ width: `${progress}%` }}
           ></div>
+
         </div>
       </div>
 
       {/* Board */}
-      <div className="flex gap-8 justify-between max-w-6xl mx-auto">
+      <div className="flex gap-8 justify-center flex-wrap max-w-7xl mx-auto">
+
         <Column
           title="Todo"
           tickets={tickets.todo}
           moveTicket={(i) => moveTicket("todo", "inProgress", i)}
-          deleteTicket={(i) => deleteTicket("todo", i)}
-          addTicket={(text, priority, tag, deadline) =>
-            addTicketToColumn("todo", text, priority, tag, deadline)
+          deleteTicket={(ticketId) => deleteTicket("todo", ticketId)}
+          addTicket={(title, priority, tag) =>
+            addTicketToColumn("todo", title, priority, tag)
           }
         />
 
@@ -116,23 +163,154 @@ function KanbanBoard() {
           title="In Progress"
           tickets={tickets.inProgress}
           moveTicket={(i) => moveTicket("inProgress", "done", i)}
-          deleteTicket={(i) => deleteTicket("inProgress", i)}
-          addTicket={(text, priority, tag, deadline) =>
-            addTicketToColumn("inProgress", text, priority, tag, deadline)
+          deleteTicket={(ticketId) => deleteTicket("inProgress", ticketId)}
+          addTicket={(title, priority, tag) =>
+            addTicketToColumn("inProgress", title, priority, tag)
           }
         />
 
         <Column
           title="Done"
           tickets={tickets.done}
-          deleteTicket={(i) => deleteTicket("done", i)}
-          addTicket={(text, priority, tag, deadline) =>
-            addTicketToColumn("done", text, priority, tag, deadline)
+          deleteTicket={(ticketId) => deleteTicket("done", ticketId)}
+          addTicket={(title, priority, tag) =>
+            addTicketToColumn("done", title, priority, tag)
           }
         />
+
       </div>
     </div>
   );
 }
 
-export default KanbanBoard;
+export default KanbanBoard;// import { tab } from "@testing-library/user-event/dist/tab";
+
+
+
+
+
+// import Column from "./Column";
+// import { useState } from "react";
+// import { useEffect } from "react";
+
+// function KanbanBoard() {
+//   const [tickets, setTickets] = useState({
+//           todo: [],
+//           inProgress: [],
+//           done: [],
+        
+//   });
+
+//   useEffect(() => {
+//   fetch("http://localhost:8080/tickets")
+//     .then((res) => res.json())
+//     .then((data) => {
+
+//       const organizedTickets = {
+//         todo: data.filter((t) => t.status === "Todo"),
+//         inProgress: data.filter((t) => t.status === "In Progress"),
+//         done: data.filter((t) => t.status === "Done"),
+//       };
+
+//       setTickets(organizedTickets);
+//     })
+//     .catch((err) => console.error(err));
+// }, []);
+
+//   // Move ticket
+//   const moveTicket = (from, to, index) => {
+//     const item = tickets[from][index];
+
+//     const updatedFrom = tickets[from].filter((_, i) => i !== index);
+//     const updatedTo = [...tickets[to], item];
+
+//     setTickets({
+//       ...tickets,
+//       [from]: updatedFrom,
+//       [to]: updatedTo,
+//     });
+//   };
+
+//   // Add ticket
+//   const addTicketToColumn = (column, text, priority, tag, deadline) => {
+//     const newTicket = {
+//       text,
+//       tag,
+//       deadline,
+//       priority: priority || "Low",
+//     };
+
+//     setTickets({
+//       ...tickets,
+//       [column]: [...tickets[column], newTicket],
+//     });
+//   };
+
+//   // Delete ticket
+//   const deleteTicket = (column, index) => {
+//     const updated = tickets[column].filter((_, i) => i !== index);
+
+//     setTickets({
+//       ...tickets,
+//       [column]: updated,
+//     });
+//   };
+
+//   const totalTasks =
+//     tickets.todo.length + tickets.inProgress.length + tickets.done.length;
+
+//   const completedTasks = tickets.done.length;
+
+//   const progress =
+//     totalTasks === 0 ? 0 : Math.round((completedTasks / totalTasks) * 100);
+
+//   return (
+//     <div className="p-6 bg-gray-100 min-h-scren">
+//       {/* Progress */}
+//       <div className="mb-6 max-w-4xl mx-auto">
+//         <h2 className="text-lg font-semibold mb-2">Progress: {progress}%</h2>
+
+//         <div className="w-full bg-gray-300 rounded-full h-4">
+//           <div
+//             className="h-3 rounded-full transition-all duration-300 bg-gradient-to-r from-green-400 to-green-600"
+//             style={{ width: `${progress}%` }}
+//           ></div>
+//         </div>
+//       </div>
+
+//       {/* Board */}
+//       <div className="flex gap-8 justify-between max-w-6xl mx-auto">
+//         <Column
+//           title="Todo"
+//           tickets={tickets.todo}
+//           moveTicket={(i) => moveTicket("todo", "inProgress", i)}
+//           deleteTicket={(i) => deleteTicket("todo", i)}
+//           addTicket={(text, priority, tag, deadline) =>
+//             addTicketToColumn("todo", text, priority, tag, deadline)
+//           }
+//         />
+
+//         <Column
+//           title="In Progress"
+//           tickets={tickets.inProgress}
+//           moveTicket={(i) => moveTicket("inProgress", "done", i)}
+//           deleteTicket={(i) => deleteTicket("inProgress", i)}
+//           addTicket={(text, priority, tag, deadline) =>
+//             addTicketToColumn("inProgress", text, priority, tag, deadline)
+//           }
+//         />
+
+//         <Column
+//           title="Done"
+//           tickets={tickets.done}
+//           deleteTicket={(i) => deleteTicket("done", i)}
+//           addTicket={(text, priority, tag, deadline) =>
+//             addTicketToColumn("done", text, priority, tag, deadline)
+//           }
+//         />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default KanbanBoard;
